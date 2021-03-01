e10c6e3f734adb8b99368d739346b09eb2b33e44
Sona Gasparyan <73468206+Sona-Gasparyan@users.noreply.github.com>
2021-03-01 19:22:45 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+    let index = 1;
+    slider(index);
+    let pause = document.getElementById("svg-container")
+    play = document.getElementById("svg-s")
+
+    let timer = setInterval(function() {
+        plusSlides(1);
+    }, 2000);
+
+
+    function plusSlides(n) {
+        slider(index += n)
+    }
+
+    function currentSlide(n) {
+        slider(index = n);
+    }
+
+    function slider(n) {
+        let img = document.getElementsByClassName("img-container");
+        let dots = document.getElementsByClassName("dot");
+        if (n > img.length) {
+            index = 1;
+        }
+        if (n < 1) {
+            index = img.length;
+        }
+        for (let i = 0; i < img.length; i++) {
+            img[i].style.display = "none";
+        }
+
+        for (let i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" clicked", "");
+        }
+        img[index - 1].style.display = "block";
+
+        dots[index - 1].className += " clicked";
+    }
+
+    function clear() {
+        clearInterval(timer)
+    }
+
+
+    pause.addEventListener("click", clear)
+    play.addEventListener("click", function() {
+        timer = setInterval(function() {
+            plusSlides(1)
+        }, 2000)
+    });
